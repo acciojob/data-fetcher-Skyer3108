c48883515f4e3733dc1075734b64f2c4bb1b6187
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-07-21 20:08:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,42 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const [dat,setData]=useState([])
+  const [loading,setLoading]=useState(true)
+
+
+  useEffect(()=>{
+
+    const fetchData=async()=>{
+
+      try{
+
+        const res=await axios.get(' https://dummyjson.com/products')
+
+        setData(res.data)
+  
+        console.log(res)
+
+      }catch(err){
+        console.log('Error fetching data:',err)
+      }finally{
+        setLoading(false)
+      }
+
+     
+    }
+
+    fetchData()
+
+  },[])
   return (
     <div>
         {/* Do not remove the main div */}
+     <h1>Data Fetched from API</h1>
+
+  {
+    loading?(<p>Loading....</p>):<pre>{JSON.stringify(dat,null,2)}</pre>
+  }
     </div>
   )
 }
